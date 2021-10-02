8eecdbfdd889a7209990828e2ed1645c864c4717
Preston So <pso@post.harvard.edu>
2021-10-02 13:52:20 -0300

    Add console.log statement and page query

--- src/pages/file-list.js
+++ src/pages/file-list.js
@@ -4,13 +4,32 @@ import { graphql, Link } from "gatsby"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 
-const FileListPage = () => (
-  <Layout>
-    <SEO title="File list" />
-    <h1>Hi from the file list page</h1>
-    <p>Welcome to the file list page</p>
-    <Link to="/">Go back to the homepage</Link>
-  </Layout>
-)
+const FileListPage = ({ data }) => {
+  console.log(data)
+  return (
+    <Layout>
+      <SEO title="File list" />
+      <h1>Hi from the file list page</h1>
+      <p>Welcome to the file list page</p>
+      <Link to="/">Go back to the homepage</Link>
+    </Layout>
+  )
+}
+
+export const query = graphql`
+  {
+    allFile {
+      edges {
+        node {
+          relativePath
+          birthTime(fromNow: true)
+          modifiedTime(fromNow: true)
+          extension
+          prettySize
+        }
+      }
+    }
+  }
+`
 
 export default FileListPage
