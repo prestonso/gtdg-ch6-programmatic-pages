9832f0d33cd3e4fce465145ce3f6301a5c071ba6
Preston So <pso@post.harvard.edu>
2021-10-02 13:49:55 -0300

    Add file-list.js

---
+++ src/pages/file-list.js
@@ -0,0 +1,16 @@
+import React from "react"
+import { Link } from "gatsby"
+
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+
+const FileListPage = () => (
+  <Layout>
+    <SEO title="File list" />
+    <h1>Hi from the file list page</h1>
+    <p>Welcome to the file list page</p>
+    <Link to="/">Go back to the homepage</Link>
+  </Layout>
+)
+
+export default FileListPage
