75c5776b086e8174df97eba59177a27213092440
Preston So <pso@post.harvard.edu>
2021-10-02 13:53:39 -0300

    Add table for file-list.js

--- src/pages/file-list.js
+++ src/pages/file-list.js
@@ -5,12 +5,32 @@ import Layout from "../components/layout"
 import SEO from "../components/seo"
 
 const FileListPage = ({ data }) => {
-  console.log(data)
   return (
     <Layout>
       <SEO title="File list" />
       <h1>Hi from the file list page</h1>
-      <p>Welcome to the file list page</p>
+      <table>
+        <thead>
+          <tr>
+            <th>Relative path</th>
+            <th>Created</th>
+            <th>Modified</th>
+            <th>Extension</th>
+            <th>Pretty size</th>
+          </tr>
+        </thead>
+        <tbody>
+          {data.allFile.edges.map( ({ node }, index) => (
+          <tr key={index}>
+            <td>{node.relativePath}</td>
+            <td>{node.birthTime}</td>
+            <td>{node.modifiedTime}</td>
+            <td>{node.extension}</td>
+            <td>{node.prettySize}</td>
+          </tr>
+          ))}
+        </tbody>
+      </table>
       <Link to="/">Go back to the homepage</Link>
     </Layout>
   )
